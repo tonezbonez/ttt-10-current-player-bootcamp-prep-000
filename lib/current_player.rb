e4110ea3e7e2f1261feba4c778d767964ed9d6e9
Anthony Bracewell <anthonybracewell.violinist@gmail.com>
2017-12-05 18:56:11 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,9 +1,10 @@
 def turn_count(board)
   counter = 0
   board.each do |count|
-    if count == "X" || "O"
+    if count == "X"
       counter += 1
-
+    elseif count == "O"
+      counter += 1
     end
   end
   counter
