d5618940741d9fd5e3d2349dbbd5be319bc159cb
Anthony Bracewell <anthonybracewell.violinist@gmail.com>
2017-12-05 18:59:03 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -9,3 +9,11 @@ def turn_count(board)
   end
   counter
 end
+
+def current_player(board)
+  if turn_count(board) % 2 == 0
+    return "X"
+  elsif turn_count(board) % 2 != 0
+    return "O"
+  end
+end
